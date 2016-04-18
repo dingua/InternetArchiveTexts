a5b67e41b5aefe0183db8edbf763a12595b15f4b
Mejdi Lassidi <mejdi.dingua@gmail.com>
2016-04-18 14:37:04 +0400

    add sorting feature to search list

--- Archive/IAHomeVC.swift
+++ Archive/IAHomeVC.swift
@@ -52,4 +52,30 @@ class IAHomeVC: UIViewController,UISearchBarDelegate,UIGestureRecognizerDelegate
             listBooksVC!.loadList(text, type: .Text)
         }
     }
+    
+    //MARK: IBAction
+    
+    @IBAction func showSortList(sender: AnyObject) {
+        let sortListVC = self.storyboard!.instantiateViewControllerWithIdentifier("sortListVC") as! IASortListVC
+        sortListVC.transitioningDelegate = listBooksVC!.sortPresentationDelegate;
+        sortListVC.modalPresentationStyle = .Custom;
+        sortListVC.delegate = listBooksVC
+        if let selectedSortOption = listBooksVC!.selectedSortDescriptor {
+            sortListVC.selectedOption = selectedSortOption
+        }
+        self.presentViewController(sortListVC, animated:true, completion:nil)
+    }
+    
+    @IBAction func changeSortDirection(sender: AnyObject) {
+        let button = sender as! UIBarButtonItem
+        listBooksVC!.descendantSort = !listBooksVC!.descendantSort
+        if let selectedSort = listBooksVC!.selectedSortDescriptor {
+            listBooksVC!.selectedSortDescriptor = selectedSort
+        }
+        if listBooksVC!.descendantSort == true {
+            button.image = UIImage(named: "down_sort")
+        }else {
+            button.image = UIImage(named: "up_sort")
+        }
+    }
 }
