d09ce6ef2c7016e51d4e1bf8095f51043cc634f3
Mejdi Lassidi <mejdi.dingua@gmail.com>
2016-05-14 08:32:11 +0400

    add Page Object to core data

---
+++ Archive/Model/Page+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  Page+CoreDataProperties.swift
+//  Archive
+//
+//  Created by Mejdi Lassidi on 5/12/16.
+//  Copyright © 2016 Archive. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Page {
+
+    @NSManaged var number: String?
+    @NSManaged var isBookmarked: NSNumber?
+    @NSManaged var chapter: Chapter?
+
+}
---
+++ Archive/Model/Page.swift
@@ -0,0 +1,17 @@
+//
+//  Page.swift
+//  Archive
+//
+//  Created by Mejdi Lassidi on 5/12/16.
+//  Copyright © 2016 Archive. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Page: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
