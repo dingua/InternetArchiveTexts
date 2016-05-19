099b9ffa7312a7f7754e6d89797b90bcea861990
Islam <qalandarov@me.com>
2016-05-19 10:16:53 +0400

    Minor UI bug fix and improvements

--- Archive/Views/IACollectionsExploreViewCell.swift
+++ Archive/Views/IACollectionsExploreViewCell.swift
@@ -12,18 +12,19 @@ class IACollectionsExploreViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var numberItemsLabel: UILabel!
     
-    //MARK: - Cell Configuration
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
     func configureWithItem(collection: ArchiveItem) {
+        titleLabel.text = collection.title
+        imageView.af_setImageWithURL(NSURL(string: "\(imageBaseURL)\(collection.identifier!)")!)
         
-        self.titleLabel.text = collection.title
-        self.imageView.image = nil
-        self.imageView.af_setImageWithURL(NSURL(string: "\(imageBaseURL)\(collection.identifier!)")!)
-        
-        self.imageView.layer.cornerRadius = Utils.isiPad() ? self.imageView.frame.size.width/2 :  50
-        self.imageView.clipsToBounds = true
-        self.imageView.layer.borderWidth = 1.0
-        self.imageView.layer.borderColor = UIColor.blackColor().CGColor
+        imageView.layer.cornerRadius = Utils.isiPad() ? imageView.frame.size.width/2 :  50
+        imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 1.0
+        imageView.layer.borderColor = UIColor.blackColor().CGColor
     }
 }
