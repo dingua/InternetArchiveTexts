7de75727de23f265809807982b099ffde6ca37c3
Islam <qalandarov@me.com>
2016-05-24 15:52:00 +0400

    Minor refactor

--- Archive/Utils/IARootVCProtocol.swift
+++ Archive/Utils/IARootVCProtocol.swift
@@ -8,34 +8,32 @@
 
 import UIKit
 
-@objc
-protocol IARootVCProtocol: class {
+@objc protocol IARootVCProtocol: class {
     optional func updateNavigationItem()
-    @objc optional func logout()
-    @objc  func logoutAction()
-
+    optional func logout()
+    func logoutAction()
 }
 
 extension IARootVCProtocol where Self: UIViewController{
     func updateNavigationItem() {
-        if Utils.isLoggedIn() {
-            let logoutBtn = UIBarButtonItem( image: UIImage(named: "logout"), landscapeImagePhone: nil, style: .Plain, target: self, action: #selector(IARootVCProtocol.logoutAction))
-            self.navigationItem.leftBarButtonItem = logoutBtn
-        }else {
+        guard Utils.isLoggedIn() else {
             self.navigationItem.leftBarButtonItem = nil
+            return
         }
+        
+        let logoutBtn = UIBarButtonItem(image: UIImage(named: "logout"), style: .Plain, target: self, action: #selector(IARootVCProtocol.logoutAction))
+        self.navigationItem.leftBarButtonItem = logoutBtn
     }
     
     func logout() {
-        let alertView = UIAlertController(title: "Logout", message: "Do you want to logout ?", preferredStyle: .Alert)
-        alertView.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { (_) in
+        let alertView = UIAlertController(title: "Logout", message: "Do you want to logout?", preferredStyle: .Alert)
+        
+        alertView.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { _ in
             IALoginManager.logout()
             self.navigationItem.leftBarButtonItem = nil
         }))
-        alertView.addAction(UIAlertAction(title: "No", style: .Cancel, handler: {_ in}))
+        
+        alertView.addAction(UIAlertAction(title: "No", style: .Cancel, handler: nil))
         self.presentViewController(alertView, animated: true, completion: nil)
     }
 }
-
-
-
